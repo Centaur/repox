93449dcbc2abd694da46f5a837c41cb9d4148c7b
xiefei <xiefei@gtan.com>
2015-10-08 21:30:34 +0800

    suppress Match not exhaustive warnings

--- src/main/scala/com/gtan/repox/config/ConfigFormats.scala
+++ src/main/scala/com/gtan/repox/config/ConfigFormats.scala
@@ -1,54 +1,64 @@
 package com.gtan.repox.config
 
-import com.gtan.repox.data.{ProxyServer, Connector, Repo}
+import com.gtan.repox.data.{Connector, ProxyServer, Repo}
 import play.api.libs.json._
 
 trait ConfigFormats {
   implicit val connectorUsageFormat = new Format[Map[Repo, Connector]] {
     override def writes(o: Map[Repo, Connector]) = JsArray(o.map {
       case (repo, connector) => JsObject(
-        Seq(
-          "repo" -> Json.toJson(repo),
-          "connector" -> Json.toJson(connector)
-        )
-      )
+                                          Seq(
+                                               "repo" -> Json.toJson(repo),
+                                               "connector" -> Json.toJson(connector)
+                                             )
+                                        )
     }.toSeq)
 
-    override def reads(json: JsValue) = json match {
-      case JsArray(values) =>
-        JsSuccess(values.map {
-          case obj: JsObject => obj.fields match {
-            case Seq(
-            ("repo", repoJsVal: JsValue),
-            ("connector", connectorJsVal: JsValue)) =>
-              repoJsVal.as[Repo] -> connectorJsVal.as[Connector]
-          }
-        }.toMap)
-      case _ => JsError("Config.connectorUsage deserialize from json failed.")
+    override def reads(json: JsValue): JsResult[Map[Repo, Connector]] = try {
+      (json: @unchecked) match {
+        case JsArray(values) =>
+          JsSuccess(values.map { value =>
+            (value: @unchecked) match {
+              case obj: JsObject => (obj.fields: @unchecked) match {
+                case Seq(
+                ("repo", repoJsVal: JsValue),
+                ("connector", connectorJsVal: JsValue)) =>
+                  repoJsVal.as[Repo] -> connectorJsVal.as[Connector]
+              }
+            }
+          } toMap)
+      }
+    } catch {
+      case e: MatchError => JsError(s"Config.connectorUsage deserialize from json failed. $e")
     }
   }
 
   implicit val proxyUsageFormat = new Format[Map[Connector, ProxyServer]] {
     override def writes(o: Map[Connector, ProxyServer]) = JsArray(o.map {
       case (connector, proxy) => JsObject(
-        Seq(
-          "connector" -> Json.toJson(connector),
-          "proxy" -> Json.toJson(proxy)
-        )
-      )
+                                           Seq(
+                                                "connector" -> Json.toJson(connector),
+                                                "proxy" -> Json.toJson(proxy)
+                                              )
+                                         )
     }.toSeq)
 
-    override def reads(json: JsValue) = json match {
-      case JsArray(values) =>
-        JsSuccess(values.map {
-          case obj: JsObject => obj.fields match {
-            case Seq(
-            ("connector", connectorJsVal: JsValue),
-            ("proxy", proxyJsVal: JsValue)) =>
-              connectorJsVal.as[Connector] -> proxyJsVal.as[ProxyServer]
-          }
-        }.toMap)
-      case _ => JsError("Config.proxyUsage deserialize from json failed.")
+    override def reads(json: JsValue): JsResult[Map[Connector, ProxyServer]] = try {
+      (json: @unchecked) match {
+        case JsArray(values) =>
+          JsSuccess(values.map { value =>
+            (value: @unchecked) match {
+              case obj: JsObject => (obj.fields: @unchecked) match {
+                case Seq(
+                ("connector", connectorJsVal: JsValue),
+                ("proxy", proxyJsVal: JsValue)) =>
+                  connectorJsVal.as[Connector] -> proxyJsVal.as[ProxyServer]
+              }
+            }
+          } toMap)
+      }
+    } catch {
+      case e: MatchError => JsError(s"Config.proxyUsage deserialize from json failed. $e")
     }
   }
 
